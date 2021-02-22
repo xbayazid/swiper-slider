5be338ea12aee9dead6e655c1c2ea72d3e064e69
unknown <bayazid9006@gmail.com>
2021-02-22 17:29:10 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    const swiper = new Swiper('.swiper-container', {
+        effect: 'cube',
+      grabCursor: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      speed: 1000,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+          },
+
+          scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: true,
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    });
+});
